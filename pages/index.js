8f0cd5be62f07f8d0e164043109d15cb265708fb
황동현 <dev_donghyun@kakao.com>
2022-01-12 15:08:11 +0900

    #2.1 Fetching Data

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,30 @@
+import { useEffect, useState } from "react";
 import Seo from "../components/Seo";
 
+const API_KEY = "98df3995db95157643c0c3d1d5872bc5";
+
 const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+      );
+      const data = await response.json();
+      setMovies(data.results);
+    })();
+  }, []);
+
   return (
     <div>
       <Seo title="Home" />
-      <h1>Home</h1>
+      {!movies && <h4>Loading...</h4>}
+      {movies?.map((movie) => (
+        <div key={movie.id}>
+          <h4>{movie.original_title}</h4>
+        </div>
+      ))}
     </div>
   );
 };
